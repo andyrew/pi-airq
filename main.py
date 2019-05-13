0db751c2ba52f9293ff541555041834bcb68723f
andyrewm <amcneil@kinestral.com>
2019-05-12 18:17:25 -0700

    adding aio data logging

---
+++ main.py
@@ -0,0 +1,37 @@
+import json
+import datetime
+from apscheduler.schedulers.blocking import BlockingScheduler
+from Adafruit_IO import Client, Feed, Data, RequestError
+
+import PMS5003
+
+
+#get key stored separately for secrecy.
+with open('aio_config.json') as jsonfile:
+    aio_config = json.load(jsonfile)
+
+ADAFRUIT_IO_KEY = aio_config["aio_key"]
+ADAFRUIT_IO_USERNAME = aio_config["aio_username"]
+
+# Create an instance of the REST client.
+aio = Client(ADAFRUIT_IO_USERNAME, ADAFRUIT_IO_KEY)
+try:
+    PM25 = aio.feeds('pm2-dot-5')
+except RequestError:
+    feed = Feed(name="pm2-dot-5")
+    PM25 = aio.create_feed(feed)
+
+# Create instance of PMS5003 object
+aiq_sensor = PMS5003.PMS5003(serial_terminal="/dev/serial0") 
+
+
+# Function for cron scheduler
+def post_data:
+    aiq_sensor.read()
+    aio.append(PM25.key, aiq_sensor.pm25_standard)
+
+
+# Cron like python scheduler
+sched = BlockingScheduler()
+sched.add_job(post_data,'cron',year='*',month='*',day='*',week='*',day_of_week='*',hour='*', minute='0/5', second='0')
+sched.start()
